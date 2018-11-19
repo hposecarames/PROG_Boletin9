2ee3d19975e7aadeb397acfb3d2fb679e229eb38
menuven <menuven@trapallada>
2018-11-19 11:02:12 +0100

    Boletin9_3

---
+++ Boletin9_1/src/boletin9_1/Boletin9_1.java
@@ -0,0 +1,18 @@
+
+package boletin9_1;
+
+/**
+ *
+ * @author menuven
+ */
+public class Boletin9_1 {
+
+    
+    public static void main(String[] args) {
+        
+        NumerosNegativos obx = new NumerosNegativos();
+        obx.datos();
+        obx.mostrarDatos();
+    }
+    
+}
---
+++ Boletin9_1/src/boletin9_1/NumerosNegativos.java
@@ -0,0 +1,37 @@
+package boletin9_1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author menuven
+ */
+public class NumerosNegativos {
+
+    int i;
+    int numero;
+    int contadorP;
+    int contadorN;
+    int cero;
+
+    public void datos() {
+
+        for (i = 0; i < 10; i++) {
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
+            if (numero < 0) {
+                contadorN++;
+            } else if (numero > 0) {
+                contadorP++;
+            } else {
+                cero++;
+            }
+
+        }
+    }
+
+    public void mostrarDatos() {
+        JOptionPane.showMessageDialog(null, "De los 10 números: " + '\n' + contadorP + " son positivos"
+                + '\n' + contadorN + " son negativos" + '\n' + cero + " son ceros");
+    }
+
+}
