f49135b11d50a0f7686efbac52fb98ec8ee3d0a3
menuven <menuven@trapallada>
2018-11-19 11:52:44 +0100

    Boletin9_1 Variable i declarada en el for

--- Boletin9_1/src/boletin9_1/NumerosNegativos.java
+++ Boletin9_1/src/boletin9_1/NumerosNegativos.java
@@ -8,7 +8,6 @@ import javax.swing.JOptionPane;
  */
 public class NumerosNegativos {
 
-    int i;
     int numero;
     int contadorP;
     int contadorN;
@@ -16,7 +15,7 @@ public class NumerosNegativos {
 
     public void datos() {
 
-        for (i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
             if (numero < 0) {
                 contadorN++;
